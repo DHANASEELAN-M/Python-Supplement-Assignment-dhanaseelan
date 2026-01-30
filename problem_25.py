4dcf911a6604cd4aeec1bafdc41bd8ab12d5011d
DHANASEELAN-M <seelandhana32@gmail.com>
2026-01-30 17:53:07 +0530

    updated python_25.py

--- problem_25.py
+++ problem_25.py
@@ -3,9 +3,9 @@
 
 def gcd(a, b):
     while b != 0:
-        temp = b
-        b = a % b
-        a = temp
+        temp=b
+        b=a % b
+        a=temp
     return a
 
 print(f"GCD of 48 and 18: {gcd(48, 18)}")
