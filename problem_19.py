8770f6380ca3c74f22433b3af032c3835f95ee2e
DHANASEELAN-M <seelandhana32@gmail.com>
2026-01-30 16:04:14 +0530

    changed promlem_19.py file

--- problem_19.py
+++ problem_19.py
@@ -1,8 +1,8 @@
 # Problem 19: Calculate power of a number
 # Find and fix the error
 
-def power(base, exponent):
-    result = 1
+def power(base,exponent):
+    result=1
     for i in range(exponent):
         result *= base
     return result
