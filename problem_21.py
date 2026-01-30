f95e1288470f07641715ca15dadba39d8fce086e
DHANASEELAN-M <seelandhana32@gmail.com>
2026-01-30 16:06:27 +0530

    changed promlem_21.py file

--- problem_21.py
+++ problem_21.py
@@ -3,9 +3,9 @@
 
 def is_sorted(lst):
     for i in range(len(lst) - 1):
-        if lst[i] > lst[i + 1]:
+        if lst[i]>lst[i + 1]:
             return False
     return True
 
-numbers = [1, 2, 3, 4, 5]
+numbers =[1, 2, 3, 4, 5]
 print(f"Is sorted: {is_sorted(numbers)}")
