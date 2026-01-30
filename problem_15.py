4bdc3b515b18eea84fd5aa9839cde49425db6c85
DHANASEELAN-M <seelandhana32@gmail.com>
2026-01-30 15:55:56 +0530

    changed promlem_15.py file

--- problem_15.py
+++ problem_15.py
@@ -2,9 +2,9 @@
 # Find and fix the error
 
 def celsius_to_fahrenheit(celsius):
-    fahrenheit = celsius * 9/5 + 32
+    fahrenheit=celsius * 9/5 + 32
     return fahrenheit
 
 temp_c = 25
 temp_f = celsius_to_fahrenheit(temp_c)
-print(f"{temp_c}°C = {temp_f}°F")
+print(f"{temp_c}°C={temp_f}°F")
